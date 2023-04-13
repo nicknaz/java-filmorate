35c373823769037d32fcefe9736ac5a71390fc8d
NNazarov <kolya.nazarov41@gmail.com>
2023-04-14 02:21:03 +0300

    refactor "all storage in services"

--- src/main/java/ru/yandex/practicum/filmorate/config/AspectJConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
-
-@ComponentScan
-@EnableSpringConfigured
-public class AspectJConfig {
-}
--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -1,27 +1,55 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.ApplicationContext;
+import ru.yandex.practicum.filmorate.dao.GenreDbStorage;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Getter
 @Setter
-@EqualsAndHashCode
+@Configurable
 public class Genre {
     @NotNull
     int id;
+    @NotBlank
     String name;
+    GenreDbStorage genreDbStorage;
+
+    private static ApplicationContext applicationContext;
+
+    public static void setApplicationContext(ApplicationContext context) {
+        applicationContext = context;
+    }
 
     public Genre(int id) {
         this.id = id;
+        GenreDbStorage genreDbStorage  = (GenreDbStorage)applicationContext.getBean(GenreDbStorage.class);
+        this.name = genreDbStorage.getGenreName(id);
     }
 
     public Genre(int id, String name) {
         this.id = id;
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Genre)) return false;
+        Genre genre = (Genre) o;
+        return id == genre.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
     /*COMEDY,
     HORROR,
     DOCUMENTARY,
